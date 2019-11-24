209e557dc5456c2ac2177583beb23c9d870f80ab
Soji <ahsojitra@gmail.com>
2019-11-24 17:57:20 -0500

    Prerefactor of list item selenium test

--- functional_tests.py
+++ functional_tests.py
@@ -30,12 +30,16 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('User peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
-                )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Buy peacock feathers to make a fly', [row.text for row in rows])
+        
+        
         self.fail('Finish the test!')
         
 
